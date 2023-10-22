37d498e613b513c028d7830cef0cadaf11108c43
mashrur-rumon <mh.rumon10@gmail.com>
2023-10-22 23:31:24 +0600

    main body content updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,6 +24,24 @@ function Home() {
             </div>
           </div>
         </div>
+        <div className='container-fluid containerPadding'>
+          <div className='row'>
+            <div className='col-md-6 bestWorkoutConrentDiv'>
+              <p className='category-heading customHeading'>
+                Best full body workout to lose fat
+              </p>
+              <p className='text-1'>
+                Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercising, either alone or with your friends at home. 
+              </p>
+              <button className='d-flex justify-content-between align-items-center px-3 blue-button'>Get Started
+                <img className='rightArrown' src={arrowRight} alt='arrowRight'/>
+              </button>
+            </div>
+            <div className='col-md-6 imageMargin'>
+              < img src={image1} alt='image1'/>
+            </div>
+          </div>
+        </div>
     </Layout>
   )
 }
