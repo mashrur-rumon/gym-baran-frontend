70ce1ae0d6959a8329552e0b202342bc5af2a6fc
mashrur-rumon <mh.rumon10@gmail.com>
2023-11-26 21:25:44 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-lg-3'>
+esrgd
+        </div>
+        <div className='col-lg-9'>
+wsefg
+        </div>
+      </div>
+    </div>
   )
 }
 
