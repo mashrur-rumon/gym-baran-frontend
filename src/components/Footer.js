2316eeea0ab0ad20f30a86800eedd47a645d5701
mashrur-rumon <mh.rumon10@gmail.com>
2023-12-21 15:19:57 +0600

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,87 +4,49 @@ function Footer() {
   return (
     <div className="fullFooter align-items-center d-flex justify-content-center">
   <div className="container">
-    <div className="footer__logo row">
+    <div className="row">
       <div className="col-md-4 col-sm-4 col-xs-4">
-      <div>
+      <div className="footer__logo">
         <span className="footerLogoFirstPart">Gym</span>
         <span className="footerLogoSecondPart">baran</span>
       </div>
-      </div>
-      <div className="col-md-8 col-sm-4 col-xs-4"></div>
-    </div>
-    <div className="row">
-      <div className="col-md-3 col-sm-4 col-xs-4">
+      <div>
         <p className="card-text">We take care of your health with regular and fun exercise</p>
       </div>
-      <div className="col-md-9 col-sm-12 col-xs-4"> 
+      </div>
+      <div className="col-md-8 col-sm-8 col-xs-8"> 
         <div className="footer__info">
-          <div className="comapany__column">
-            <h5 className="heading">Program</h5>
-            <ul>
+          <div className="pb-4">
+            <h5 className="footerHeading">Program</h5>
+            <ul className='footerSubInfo'>
               <li>Workout</li>
               <li>Gym</li>
               <li>Cardio</li>
               <li>Zumba</li>
           </ul>
           </div>
-          {/* <div className="influencer__part d-flex"> */}
-            <div className="influencer__div">
-              <h5 className="heading">Blog</h5>
-              <ul>
+            <div className="pb-4">
+              <h5 className="footerHeading">Blog</h5>
+              <ul className='footerSubInfo'>
                 <li>Daily Stretch</li>
                 <li>Daily Workout</li>    
               </ul>
             </div>
-            <div className="advisor__div">
-              <h5 className="heading">About Us</h5>
-              <ul>
+            <div>
+              <h5 className="footerHeading">About Us</h5>
+              <ul className='footerSubInfo'>
                 <li>Support</li>
                 <li>Contact</li>
                 <li>Address</li>
              </ul>
             </div>
-          {/* </div> */}
           <div>            
           </div>
         </div>     
       </div>
+    </div>
   </div>
 </div>
-</div>
-    // <div className='container-fluid fullFooter'>
-    //   <div className='row'>
-    //     <div className='col-lg-4 footerFirstPartDiv'>
-    //       <div>
-    //         <span className="logoFirstPart">Gym</span>
-    //         <span className="logoSecondPart">baran</span>
-    //       </div>
-    //       <span>We take care of your health with regular and fun exercise</span>
-    //     </div>
-    //     <div className='col-lg-8'>
-    //       <ul>
-    //         <li> 
-    //           <span>Program</span>
-    //           <span>Workout</span>
-    //           <span>Gym</span>
-    //           <span>Cardio</span>
-    //           <span>Zumba</span>
-    //         </li>
-    //         <li>
-    //           <span>Blog</span>
-    //           <span>Faily Stretch</span>
-    //           <span>Daily Workout</span>
-    //         </li>
-    //         <li>
-    //           <span>About Us</span>
-    //           <span>Support</span>
-    //           <span>Contact</span>
-    //           <span>Address</span>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
